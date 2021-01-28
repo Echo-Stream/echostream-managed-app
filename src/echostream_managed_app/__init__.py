3a20092cba3539640cf112d2140e167fafbf1d53
Joseph Wortmann <joseph.wortmann@gmail.com>
2021-01-28 16:10:15 -0600

    get TENANT from environ

--- src/echostream_managed_app/__init__.py
+++ src/echostream_managed_app/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3.8
 import asyncio
 import functools
-import json
 from base64 import b64decode
 from datetime import datetime
 from hashlib import sha256
@@ -22,7 +21,6 @@ from docker.types import Healthcheck, LogConfig, Mount
 from docker_image.reference import Reference
 from gql import Client, gql
 from gql.transport.aiohttp import AIOHTTPTransport
-from jose import jwt
 from watchtower import CloudWatchLogHandler
 
 from .appsync_websockets import AppSyncOIDCAuthorization, AppSyncWebsocketsTransport
@@ -55,7 +53,6 @@ handler.setFormatter(formatter)
 getLogger().handlers = [handler]
 getLogger().setLevel(environ.get("LOG_LEVEL") or INFO)
 TOKEN_FETCHER = TokenFetcher()
-TENANT = None
 NODES = {}
 DOCKER = from_env()
 ECR_PATTERN = compile(
@@ -65,13 +62,14 @@ ECR = client("ecr")
 NODE_ENV_VARS = {
     "APPSYNC_ENDPOINT": environ["APPSYNC_ENDPOINT"],
     "AUDIT_FIREHOSE": environ["AUDIT_FIREHOSE"],
+    "AWS_DEFAULT_REGION": environ["AWS_DEFAULT_REGION"],
     "COGNITO_APP_ID": environ["COGNITO_APP_ID"],
     "COGNITO_IDENTITY_POOL_ID": environ["COGNITO_IDENTITY_POOL_ID"],
     "COGNITO_PASSWORD": environ["COGNITO_PASSWORD"],
     "COGNITO_USER_POOL_ID": environ["COGNITO_USER_POOL_ID"],
     "COGNITO_USERNAME": environ["COGNITO_USERNAME"],
     "CONTROL_REGION": environ["CONTROL_REGION"],
-    "AWS_DEFAULT_REGION": environ["AWS_DEFAULT_REGION"]
+    "TENANT": environ["TENANT"],
 }
 HEALTHCHECK_TIME_PATTERN = compile(r"^([0-9]+)(ms|s|m|h)$")
 HEALTHCHECK_TIME_MULTIPLIERS = {
@@ -96,7 +94,7 @@ def convert_healthcheck_time(value: str) -> int:
         raise ValueError(f"Value {value} is not a healthcheck time string")
 
 
-async def initialize_app(app: str) -> None:
+async def initialize_app() -> None:
     # Clean up hanging containers and images, if any
     await asyncio.gather(
         *[
@@ -159,12 +157,13 @@ async def initialize_app(app: str) -> None:
             """
         )
         app_result = await client.execute(
-            query, variable_values={"tenant": TENANT, "name": app}
+            query,
+            variable_values={"tenant": environ["TENANT"], "name": environ["APP_NAME"]},
         )
     if app_result["SearchApps"]["lastEvaluatedKey"]:
-        raise Exception(f"More than one configuration found for {app}")
+        raise Exception(f'More than one configuration found for {environ["APP_NAME"]}')
     if not app_result["SearchApps"]["items"]:
-        raise Exception(f"No configuration found for {app}")
+        raise Exception(f'No configuration found for {environ["APP_NAME"]}')
     await asyncio.gather(
         *[
             start_node(node_config["name"], node_config)
@@ -247,7 +246,7 @@ async def start_node(node: str, node_config: Optional[Dict[str, Any]] = None) ->
                     """
                 )
                 node_result = await client.execute(
-                    query, variable_values={"tenant": TENANT, "name": node}
+                    query, variable_values={"tenant": environ["TENANT"], "name": node}
                 )
             if node_result["SearchNodes"]["lastEvaluatedKey"]:
                 raise Exception(f"More than one configuration found for {node}")
@@ -267,7 +266,9 @@ async def start_node(node: str, node_config: Optional[Dict[str, Any]] = None) ->
             image_ref.repository["domain"]
         ):
             if username or password:
-                raise Exception("Username and password are mutually exclusive to using an ecr repo.")
+                raise Exception(
+                    "Username and password are mutually exclusive to using an ecr repo."
+                )
             authorization_data = await _run_in_executor(
                 functools.partial(
                     ECR.get_authorization_token,
@@ -275,11 +276,17 @@ async def start_node(node: str, node_config: Optional[Dict[str, Any]] = None) ->
                 )
             )
             username, password = (
-                b64decode(authorization_data["authorizationData"][0]["authorizationToken"]).decode().split(":")
+                b64decode(
+                    authorization_data["authorizationData"][0]["authorizationToken"]
+                )
+                .decode()
+                .split(":")
             )
 
         if (username, password) and None in (username, password):
-            raise Exception("You must provide both username AND password for private registries or neither for public registries")
+            raise Exception(
+                "You must provide both username AND password for private registries or neither for public registries"
+            )
 
         # Add the auth_config to the pull args if both username and password are present
         if username and password:
@@ -391,16 +398,8 @@ async def remove_node(node: str) -> None:
 
 async def run() -> None:
     getLogger().info(f'Starting app {environ["APP_NAME"]}')
-    claims = jwt.get_unverified_claims(TOKEN_FETCHER.id_token)
-    if "tenants" not in claims:
-        raise Exception('Invalid token, does not contain claim "tenants"')
-    tenants = json.loads(claims["tenants"])
-    if len(tenants) != 1:
-        raise Exception('Either no tenants in "tenants" claim or too many')
-    global TENANT
-    TENANT = list(tenants.keys())[0]
 
-    await initialize_app(environ["APP_NAME"])
+    await initialize_app()
 
     async with Client(
         transport=AppSyncWebsocketsTransport(
@@ -427,7 +426,7 @@ async def run() -> None:
         )
         async for notification in client.subscribe(
             subscription,
-            variable_values={"tenant": TENANT, "app": environ["APP_NAME"]},
+            variable_values={"tenant": environ["TENANT"], "app": environ["APP_NAME"]},
         ):
             if notification["itemType"] in ("app", "tenant"):
                 if notification["operation"] == "REMOVE":
